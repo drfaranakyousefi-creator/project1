2979d9ad31eac1da8998d60f34ebf66f866a6afd
drfaranakyousefi-creator <drfaranakyousefi@gmail.com>
2025-09-20 23:10:04 +0330

    Update transmitter_simulation.py

--- transmitter_simulation.py
+++ transmitter_simulation.py
@@ -5,12 +5,12 @@ import json
 
 
 class Transmitter : 
-    def __init__(self , server_url, dataset_name, device ) :   
+    def __init__(self ,w ,  server_url, dataset_name, device ) :   
         if dataset_name == 'metavision' : 
             N = 4
         else : 
             N = 5    
-        self.model = prediction_net(w=3, n_features_input=N, lr=0.01)
+        self.model = prediction_net(w, n_features_input=N, lr=0.01)
         self.device = device 
     def data_to_json(self ,  x , label ,  status)  : # x and the labels are both tensors
         x_copy = x.detach().cpu().tolist()
@@ -47,5 +47,6 @@ class Transmitter :
         elif status == 'test' :
             prediction = torch.tensor(data_recive_in_client['prediction']).to(self.device)
             return  prediction
+
 
 
