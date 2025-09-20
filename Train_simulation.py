15e78c64973f96686311be5a8f94cee3beef6146
drfaranakyousefi-creator <drfaranakyousefi@gmail.com>
2025-09-20 17:03:24 +0330

    Update Train_simulation.py

--- Train_simulation.py
+++ Train_simulation.py
@@ -18,7 +18,7 @@ class HTTPS(nn.Module) :
         self.network = client_network(w , self.N ,lr=0.01).to(device)
         chartevents_path = "/content/drive/MyDrive/split_learning/CHARTEVENTS.csv"
         df_chartevents = pd.read_csv(chartevents_path)
-        self.data = data_preparing(df_chartevents , dataset_name , w , test_size = 0.2 , target )
+        self.data = data_preparing(df_chartevents , dataset_name , w , test_size = 0.2 , target = target )
         self.transmittion = Transmitter(server_url , dataset_name, device)
         self.batch_size = batch_size 
         self.loss_fn = nn.MSELoss()
@@ -89,6 +89,7 @@ class HTTPS(nn.Module) :
             number += a 
         loss = loss / number
         return loss 
+
 
 
 
